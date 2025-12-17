466a1d22d156662cf6a5872d63ca95bd04878a05
jckirton <66995801+jckirton@users.noreply.github.com>
2025-12-17 14:25:28 +1030

    Added API URL config, improved logging system, and more detailed initialisation logs.

--- hackmudChatAPI/hackmudChatAPI.py
+++ hackmudChatAPI/hackmudChatAPI.py
@@ -14,44 +14,60 @@ class ChatAPI:
     from requests import Response
 
     def __init__(
-        self, config_file: str = f"{path[0]}/config.json", token_refresh: bool = False
+        self,
+        config_file: str = f"{path[0]}/config.json",
+        token_refresh: bool = False,
+        verbosity: int = 1,
     ):
+        self.verbosity = verbosity
         self.token_refresh = token_refresh
         self.config_file = config_file
         self.config: dict
 
+        self.log("Initialising ChatAPI instance...")
+
+        self.log(f"Attempting to load config from {repr(self.config_file)}...")
         try:
             self.load_config()
+            self.log("Config successfully loaded.")
         except FileNotFoundError:
 
-            print(f'Config file not found at "{self.config_file}".')
-            print(f'Creating new config file at "{self.config_file}".')
+            self.log(f"Config file not found at '{repr(self.config_file)}'.")
 
+            self.log("Setting base config...")
             self.config = {
-                "ErrorOnBadToken": False,
+                "url": "https://www.hackmud.com",
                 "header": {"Content-Type": "application/json"},
+                "ErrorOnBadToken": False,
             }
+
+            self.log(f"Creating new config file at '{repr(self.config_file)}'...")
             self.save_config()
+            self.log("New config file created.")
             self.load_config()
 
             self.get_token()
-            self.get_users()
 
-        if self.config.get("ErrorOnBadToken", None) is None:
-            self.config["ErrorOnBadToken"] = True
+        if self.config.get("url") is None:
+            self.log("API URL not present in config - saving base definition.")
+            self.config["url"] = "https://www.hackmud.com"
             self.save_config()
 
-        self.header: dict = self.config.get("header", None)
+        # self.header: dict = self.config.get("header")
 
         if self.header is None:
+            self.log("API header not present in config - saving base definition.")
             self.config["header"] = {"Content-Type": "application/json"}
             self.header = self.config.get("header", None)
             self.save_config()
 
-        self.token: str = self.config.get("chat_token", None)
+        if self.config.get("ErrorOnBadToken") is None:
+            self.log("ErrorOnBadToken not present in config - saving base definition.")
+            self.config["ErrorOnBadToken"] = True
+            self.save_config()
 
         if self.token is None:
-            print("No chat API token present in config.")
+            self.log("No chat API token present in config.")
             self.get_token(
                 badToken=True, BTReason="no chat API token present in config."
             )
@@ -59,9 +75,45 @@ class ChatAPI:
         token_test = self.test_token()
 
         while not token_test:
+            self.log("Token tested bad.")
             token_test = self.get_token(badToken=True, BTReason="bad token.")
 
-        self.users = self.get_users()
+        self.log("Retriving account data...")
+        self.get_users()
+        self.users = self.config.get("users")
+        self.log("Account data retrieved.")
+
+        self.log("Instance configuration:")
+        self.log(f"Config File: {repr(self.config_file)}")
+        self.log(f"API URL: {repr(self.url)}")
+        self.log(f"API Header: {repr(self.header)}")
+        self.log(f"API Token: {repr(self.token)}", 2)
+        self.log(f"Error on Bad Token: {repr(self.config.get("ErrorOnBadToken"))}")
+        self.log(f"Users: {repr(self.users)}")
+        self.log("ChatAPI instance initialised.")
+
+    def log(self, input, verbosity: int = 1, level: int = 0):
+        """
+        My logging function.
+
+        Args:
+            input (any): The thing being logged.
+            verbosity (int, optional): The verbosity level at which it will appear. Defaults to 1.
+            level (int, optional): The level/type of the log. Defaults to 0. If a level >5 is given, it will be set to 5.
+
+                The log levels (in order) are:
+                - (0) LOG
+                - (1) INFO
+                - (2) DEBUG
+                - (3) WARN
+                - (4) ERROR
+                - (5) FATAL
+        """
+        LOG_LEVELS = ["LOG", "INFO", "DEBUG", "WARN", "ERROR", "FATAL"]
+        if level > len(LOG_LEVELS) - 1:
+            level = len(LOG_LEVELS)
+        if self.verbosity >= verbosity:
+            print(f"[{LOG_LEVELS[level]}] {input}")
 
     def test_token(self, token=None):
         import requests
@@ -69,16 +121,18 @@ class ChatAPI:
         if token is None:
             token = self.token
 
+        self.log("Testing token...")
         response = requests.post(
-            url="https://www.hackmud.com/mobile/account_data.json",
+            url=f"{self.url}/mobile/account_data.json",
             headers=self.header,
             json={"chat_token": token},
         ).content
 
         if response == b"":
-            print("Token invalid.")
+            self.log("Token invalid.")
             return False
         else:
+            self.log("Token valid.")
             return True
 
     def load_config(self):
@@ -86,9 +140,9 @@ class ChatAPI:
 
         with open(self.config_file) as f:
             self.config: dict = json.load(f)
-            self.header: dict = self.config.get(
-                "header", {"Content-Type": "application/json"}
-            )
+            self.url: dict = self.config.get("url")
+            self.header: dict = self.config.get("header")
+            self.token: str = self.config.get("chat_token")
 
     def save_config(self):
         import json
@@ -133,33 +187,35 @@ class ChatAPI:
             if self.config["ErrorOnBadToken"]:
                 raise TokenError(f"Bad chat API token - {BTReason}")
             else:
-                print("Requesting new token.")
+                self.log("Getting new token (bad token)...")
+        else:
+            self.log("Getting new token...")
 
         if not chat_pass:
             chat_pass = input("chat_pass password: ")
 
         if chat_pass != "":
+            self.log("Requesting new token...")
             newToken = json.loads(
                 requests.post(
-                    url="https://www.hackmud.com/mobile/get_token.json",
+                    url=f"{self.url}/mobile/get_token.json",
                     headers=self.header,
                     json={"pass": chat_pass},
                 ).content
             ).get("chat_token", None)
 
             if self.test_token(newToken):
-                print("New token generated.")
+                self.log("New token generated.")
                 self.token = newToken
                 self.config["chat_token"] = newToken
                 self.save_config()
-                print("New token saved.")
+                self.log("New token saved.")
                 if self.token_refresh:
                     quit()
                 return True
             else:
-                print("Bad chat_pass. No new token.")
+                self.log("Bad chat_pass - no new token.")
                 self.load_config()
-                self.token = self.config.get("chat_token", None)
                 if self.token_refresh:
                     quit()
                 return False
@@ -170,17 +226,18 @@ class ChatAPI:
 
         self.load_config()
 
-        self.config["users"] = json.loads(
+        accountData: dict = json.loads(
             requests.post(
-                url="https://www.hackmud.com/mobile/account_data.json",
+                url=f"{self.url}/mobile/account_data.json",
                 headers=self.header,
                 json={"chat_token": self.token},
             ).content
         )["users"]
 
-        self.save_config()
+        self.config["users"] = list(accountData.keys())
 
-        return list(self.config["users"].keys())
+        self.save_config()
+        return accountData
 
     def send(self, user: str, channel: str, msg: str) -> Response:
         """
@@ -201,7 +258,7 @@ class ChatAPI:
         }
 
         return requests.post(
-            url="https://www.hackmud.com/mobile/create_chat.json",
+            url=f"{self.url}/mobile/create_chat.json",
             headers=self.header,
             json=payload,
         )
@@ -225,7 +282,7 @@ class ChatAPI:
         }
 
         return requests.post(
-            url="https://www.hackmud.com/mobile/create_chat.json",
+            url=f"{self.url}/mobile/create_chat.json",
             headers=self.header,
             json=payload,
         )
@@ -266,7 +323,7 @@ class ChatAPI:
 
         chats: dict = json.loads(
             requests.post(
-                url="https://www.hackmud.com/mobile/chats.json",
+                url=f"{self.url}/mobile/chats.json",
                 headers=self.header,
                 json=payload,
             ).content
